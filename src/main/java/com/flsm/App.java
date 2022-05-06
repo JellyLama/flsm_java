3a02b51a6de635248757dffe96ad7727ca3802d7
Jellylama <karimbachir2001@gmail.com>
2022-05-06 14:20:44 +0200

    adapted code to the ipv4Address class update

--- src/main/java/com/flsm/App.java
+++ src/main/java/com/flsm/App.java
@@ -4,24 +4,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
         // user input for ip and sm
         System.out.println("===================================================");
         Scanner scanner = new Scanner(System.in);
         String starterIp = "";
+        String starterSm = "";
+
         boolean valid = false;
-        while(!valid){
+        while (!valid) {
             System.out.println("-Enter ip address: ");
             starterIp = scanner.next();
             valid = ipv4Address.validateIpv4(starterIp);
 
-            if(!valid)
-                System.out.println("INVALID IP ADDRESS!");
+            if (!valid)
+                System.out.println("\nINVALID IP ADDRESS!\n");
         }
-        System.out.println("-Enter subnet mask: ");
-        String starterSm = scanner.next();
-        
+        valid = false;
+        while (!valid) {
+            System.out.println("-Enter subnet mask: ");
+            starterSm = scanner.next();
+            valid = ipv4Address.validateSubnetMask(starterSm);
+
+            if (!valid)
+                System.out.println("\nINVALID SUBNETMASK!\n");
+        }
+
         // user input for number of LANs and number of HOSTs for each LAN
         System.out.println("-Enter the number of LANs: ");
         int nLan = scanner.nextInt();
@@ -31,7 +40,7 @@ public class App {
         // user input to exclude or not the first network IP
         char excludefirstIp = '0';
         while (excludefirstIp != 'y' && excludefirstIp != 'n') {
-            System.out.println("-Do you want to exclude the first and last network IPs [y/n]? ");
+            System.out.println("-Do you want to exclude the first network IP [y/n]? ");
             excludefirstIp = scanner.next().charAt(0);
         }
         scanner.close();
@@ -54,46 +63,51 @@ public class App {
             else
                 hostId++;
         }
+
         int netId = 32 - hostId;
+        if (netId < starterAddress.getCidr()) {
+            System.out.println("\nNOT ENOUGH SPACE FOR " + hosts + " HOSTS\n");
+        } else {
 
-        unassignedSubnet.setSubnetMask(netId);
+            unassignedSubnet.setSubnetMask("" + netId);
 
-        for (int i = 0; i < nLan; i++) {
+            for (int i = 0; i < nLan; i++) {
 
-            int[] subnetMask = unassignedSubnet.getSubnetMask();
-            int[] newIp = unassignedSubnet.getNetworkId();
+                String[] splittedSm = unassignedSubnet.getSubnetMask().split("\\.");
+                String[] splittedNewIp = unassignedSubnet.getNetworkId().split("\\.");
 
-            if (excludefirstIp == 'n')
-                // adds subnet to array
-                subnets.add(subnets.size(), new ipv4Address(newIp, subnetMask));
+                if (excludefirstIp == 'n')
+                    // adds subnet to array
+                    subnets.add(subnets.size(), (ipv4Address) unassignedSubnet.clone());
 
-            // updates unassigned ip (network id for the next subnet)
-            // calculates ip's sector to update, -1 because index starts from 0
-            int sector = (netId / 8) - 1;
-            if (netId % 8 != 0)
-                sector++;
+                // calculates ip's sector to update, -1 because index starts from 0
+                int sector = (netId / 8) - 1;
+                if (netId % 8 != 0)
+                    sector++;
 
-            int newSectorValue = ((int) Math.pow(2, hostId % 8)) + newIp[sector];
+                int newSectorValue = ((int) Math.pow(2, hostId % 8)) + Integer.parseInt(splittedNewIp[sector]);
 
-            // if the value of the updated sector exceeds the max value (defined by the
-            // subnet mask) then sector--
-            while (newSectorValue > subnetMask[sector]) {
-                newIp[sector] = 0;
-                sector--;
-                newSectorValue = newIp[sector] + 1;
+                // if the value of the updated sector exceeds the max value (defined by the
+                // subnet mask) then sector--
+                while (newSectorValue > Integer.parseInt(splittedSm[sector])) {
+                    splittedNewIp[sector] = "0";
+                    sector--;
+                    newSectorValue = Integer.parseInt(splittedNewIp[sector]) + 1;
+                }
+                splittedNewIp[sector] = "" + newSectorValue;
+
+                // updates unassigned ip (network id for the next subnet)
+                unassignedSubnet.setIp(String.join(".", splittedNewIp));
+
+                if (excludefirstIp == 'y')
+                    // adds subnet to array
+                    subnets.add(subnets.size(), (ipv4Address) unassignedSubnet.clone());
             }
 
-            newIp[sector] = newSectorValue;
-            unassignedSubnet.setIp(newIp);
-
-            if (excludefirstIp == 'y')
-                // adds subnet to array
-                subnets.add(subnets.size(), new ipv4Address(newIp, subnetMask));
-        }
-
-        // prints the subnets
-        for (int i = 0; i < nLan; i++) {
-            System.out.println("[LAN " + (i + 1) + "]\n" + subnets.get(i).toString() + "\n");
+            // prints the subnets
+            for (int i = 0; i < nLan; i++) {
+                System.out.println("[LAN " + (i + 1) + "]\n" + subnets.get(i).toString() + "\n");
+            }
         }
     }
 }
